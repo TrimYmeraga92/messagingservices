5a28c95f358cea41146a9c87e67be2cea7325484
TrimYmeraga92 <71897922+TrimYmeraga92@users.noreply.github.com>
2022-11-01 17:31:28 +0100

    submit work to Github

---
+++ 1. Azure Storage Queues/Lab - Azure Storage Queue - .NET - Sending messages/StorageQueue/StorageQueue/Program.cs
@@ -0,0 +1,32 @@
+﻿using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+
+string connectionString = "DefaultEndpointsProtocol=https;AccountName=appstoragefromarm;AccountKey=loGTOoA6AdhEFv96MPVzJBa+nFFM1sKH0GogUmA08Hov2mBz0T9hiyb1YrFIsIk9k17Q+wiGVpeY+ASt7yfhfQ==;EndpointSuffix=core.windows.net";
+string queueName = "queue1";
+
+
+PeekMessage();
+
+//void sendMessage(string message)
+/{
+    QueueClient queueClient = new QueueClient(connectionString, queueName);
+    if (queueClient.Exists())
+    {
+       queueClient.SendMessage(message);
+        Console.WriteLine("Message sent {0}", message);
+    }
+}
+
+void PeekMessage()
+{
+
+    QueueClient queueclient = new QueueClient(connectionString, queueName);
+    int maxmessages = 10;
+
+    PeekedMessage[] peekMessages = queueclient.PeekMessage(maxmessages);
+    Console.WriteLine("The message in the queue are");
+    foreach( var peekMessage in peekMessages)
+    {
+        Console.WriteLine(peekMessage.Body);
+    }
+}
---
+++ 1. Azure Storage Queues/Lab - Sending Messages in base64 encoded/StorageQueue/StorageQueue/Program.cs
@@ -0,0 +1,27 @@
+﻿using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+using Newtonsoft.Json;
+using StorageQueue;
+
+string connectionString = "DefaultEndpointsProtocol=https;AccountName=appstoragefromarm;AccountKey=loGTOoA6AdhEFv96MPVzJBa+nFFM1sKH0GogUmA08Hov2mBz0T9hiyb1YrFIsIk9k17Q+wiGVpeY+ASt7yfhfQ==;EndpointSuffix=core.windows.net";
+string queueName = "queue1";
+
+
+await SendMessage("O1", 100);
+await SendMessage("O2", 200);
+await SendMessage("O3", 300);
+
+async Task SendMessage(string orderid,int quantity)
+{
+    QueueClient queueClient = new QueueClient(connectionString, queueName);
+    if (queueClient.Exists())
+    {
+       Order order=new Order {OrderID = orderid,Quantity=quantity};
+        var jsonObject = JsonConvert.SerializeObject(order);
+        var bytes= System.Text.Encoding.UTF8.GetBytes(jsonObject);
+        var message= System.Convert.ToBase64String(bytes);  
+       await queueClient.SendMessageAsync(message);
+       Console.WriteLine("Order Id {0} sent", orderid);
+    }
+}
+
